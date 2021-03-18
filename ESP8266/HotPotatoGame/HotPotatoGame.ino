43925927510dfcac6bafbbdde077db2a8841db85
isack5268 <57671827+isack5268@users.noreply.github.com>
2021-03-18 22:41:17 +0300

    logo for display was added + in ino file was added timer and comments

--- ESP8266/HotPotatoGame/HotPotatoGame.ino
+++ ESP8266/HotPotatoGame/HotPotatoGame.ino
@@ -3,7 +3,9 @@
 #include <Adafruit_SSD1306.h>
 #include <ESP8266TrueRandom.h>
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);                                                                                                                                  
+#define cButton 12 // центральная кнопка
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1); // инициализируем дисплей                                                                                                                              
 
 #define logo_width 40
 #define logo_height 40
@@ -24,56 +26,59 @@ static const unsigned char PROGMEM logo[] = {
   0x02, 0x00, 0x00, 0xF0, 0x07, 0x18, 0x00, 0x0C, 0xF0, 0x0F, 0x80, 0xFF, 
   0x00, 0xF0, 0x0F, 0x00, 0x00, 0x40, 0x78, 0x1F, 0x08, 0x00, 0x08, 0xFC, 
   0x79, 0x40, 0xF9, 0x01, 0xBE, 0xF8, 0x00, 0x00, 0x80, 0x1F, 0xF0, 0x07, 
-  0x00, 0xF0, 0xE7, 0xF0, 0xFF, 0x80, 0xFF, 0xE7, };
+  0x00, 0xF0, 0xE7, 0xF0, 0xFF, 0x80, 0xFF, 0xE7, };  // добавляем лого
 
-String words[] = {"Памперс", "Киллер", "Ассасин", "Майнкрафт", "Кондиционер", "Мама", "Бомж", "Дебил", "Карл", "Фетиш", "Игрушка", "Скрипка", "Баян", "Водка", "Пиво", "Колесо", "Машина", "Кружка", "Рот", "Батарейка"};
-const int wordsSize = sizeof(words) / sizeof(String);
-int soundPin = 13, rung = 1;
+ // словарь
+String words[] = {"подпись", "вырез", "гранит", "кругозор", "блузка", "фараон", "клапан", "ёж", "вымя", "турист", "колготки", "стоп-кран", "питание", "свёрток", "дочерь", "шампунь", "броня", "зайчатина", "гимназист", "стелька", "подделка", "виза", "затычка", "решение", "алкоголь", "шуруп", "воровка", "колодец", "кабан", "команда", "бордель", "ловушка", "буква", "опера", "сектор", "математика", "пароварка", "невезение", "глубина", "штука", "справочник", "вождь", "хобот", "ширинка", "усталость", "служитель", "жар", "спальная", "видео", "рот", "просьба", "фишка", "рукопись", "ракетчик", "каблук", "шрифт", "палец", "ножка", "халва", "черника", "незнайка", "компания", "работница", "мышь", "исследование", "кружка", "мороженое", "сиденье", "пулемёт", "печь", "солист", "свёкла", "стая", "зелье", "дума", "посылка", "коготь", "семафор", "брат", "различие", "плоскостопие", "двигатель", "сфера", "тюльпан", "затвор", "внедорожник", "самурай", "стан", "алгоритм", "параграф", "глаз", "медалист", "пульт", "поводок", "подлежащее", "ор", "бунт", "удочка", "лес", "диспетчер", "монитор", "вдова", "пиратство", "астролог", "сосед", "пуп", "изобретатель", "чума", "танец", "затишье", "пластелин", "йог", "маска", "блоха", "судьба", "сияние", "рукавица", "филе", "заплыв", "сёмга", "гиппопотам", "мастер", "походка", "ландыш", "яблоня", "кляча", "лиса", "свёртываемость", "раствор", "соты", "солод", "спорт", "шифер", "прощение", "стопка", "побег", "простота", "запах", "беседа", "варенье", "пароль", "актёр", "вырубка", "гвоздь", "шкаф", "скальпель", "гонг", "профессор", "казан", "скорбь", "извоз", "добавка", "тропа", "шеф", "космонавт", "грифель", "лауреат", "ромб", "борец", "звон", "канистра", "олимпиада", "оплата", "спирт", "баян", "перекрёсток", "влажность", "лотерея", "насморк", "оправдание", "мушкетёр", "мороз", "утюг", "санитария", "опрятность"};
+unsigned const int wordsSize = sizeof(words) / sizeof(String), soundPin = 13;  // размер словаря
+unsigned int rung = 1;
 unsigned long startTime = 0;
 bool check1 = false, timeGo = false;
 
 void setup() {
   pinMode (soundPin, OUTPUT);
-  pinMode(12, OUTPUT);
-  digitalWrite(12,HIGH);
+  pinMode(cButton, OUTPUT);
+  digitalWrite(cButton,HIGH);
+  
   Serial.begin(115200);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
+  Random(1);
+  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // определяем дисплей по адресу 0x3C
   display.clearDisplay(); 
-  display.cp437(true);
+  display.cp437(true);  // задаем сдвиг под символам на вывод
+  display.setTextColor(WHITE); 
   delay(2000);
+  
   display.drawXBitmap(128 / 2 - logo_width / 2, 0, logo, logo_width, logo_height, 1);
   display.display();
   display.setTextSize(1,2);
-  display.setTextColor(WHITE);
   display.setCursor(25, 45);
-  display.write(80); //P
-  display.write(97); //a
-  display.write(110); //n
-  display.write(99); //c
-  display.write(97); //a
-  display.write(107); //k
-  display.write(101); //e
-  display.write(70); //F
-  display.write(97); //a
-  display.write(99); //c
-  display.write(101); //e
-  display.write(115); //s
+  display.write(80);  // P
+  display.write(97);  // a
+  display.write(110); // n
+  display.write(99);  // c
+  display.write(97);  // a
+  display.write(107); // k
+  display.write(101); // e
+  display.write(70);  // F
+  display.write(97);  // a
+  display.write(99);  // c
+  display.write(101); // e
+  display.write(115); // s
   display.display();
-  delay(1500);           
-  display.setTextColor(WHITE); 
-  Random(1);
+  delay(1500);  
 }
 
 void loop() {
-  bool but1 = digitalRead(12);
+  bool but1 = digitalRead(cButton); // читаем состояние кнопки и переводим в булевую
 
-  if(rung >= wordsSize){
+  if(rung = wordsSize){
     Serial.println("Over");
     rung = 1;
     Random(1);
-  }
+  } // когда мы прошли весь словарь - решафлим его и пускаем заново
 
-  if(timeGo && millis() >= startTime + ESP8266TrueRandom.random(50, 70)){
+  if(timeGo && millis() >= startTime + ESP8266TrueRandom.random(50000, 70000)){
     startTime = 0;
     timeGo = false;
     display.clearDisplay();
@@ -86,26 +91,25 @@ void loop() {
     display.println(utf8rus("окончен"));
     display.display();;
     delay(1500);
-  }
+  } // окончание времени на раунд
 
-  if(!but1 && !check1){
+  if(!but1 && !check1){ // начало раунда по нажатию кнопки
     check1 = true;
 
     if(!timeGo){
       timeGo = true;
       startTime = millis();
-    }
+    } // начинаем отсчет
     
     Output(rung);
     rung++;
-    tone (soundPin, 600, 200);
+    tone (soundPin, 600, 200); // пиликаем
   }
   else if(but1)
     check1 = false;
 }
 
-String utf8rus(String source)
-{
+String utf8rus(String source){ // перевод латиницы в кириллицу
   int i,k;
   String target;
   unsigned char n;
@@ -134,7 +138,7 @@ String utf8rus(String source)
 return target;
 }
 
-void Output(int num){ 
+void Output(int num){  // вывод слова на экран
   display.clearDisplay();
   display.drawRoundRect (1, 1, 127, 63, 5, 1);    
   display.setTextSize(2);                  
@@ -143,7 +147,7 @@ void Output(int num){
   display.display();;
 }
 
-void Random(int i){
+void Random(int i){ // шафлим словарь
   switch(i){
     case 1:
       for (int i = 0; i < wordsSize; i++){
