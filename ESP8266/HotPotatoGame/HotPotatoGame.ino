d78a2f257afeb3ec41c867ca2a74f4c619efa536
isack5268 <57671827+isack5268@users.noreply.github.com>
2021-03-23 11:03:41 +0300

    reworked to discplay 0,91

--- ESP8266/HotPotatoGame/HotPotatoGame.ino
+++ ESP8266/HotPotatoGame/HotPotatoGame.ino
@@ -3,34 +3,23 @@
 #include <Adafruit_SSD1306.h>
 #include <ESP8266TrueRandom.h>
 
-#define cButton 12 // центральная кнопка
+#define cButton 14  // центральная кнопка
+#define soundPin 13  // пин пищалки
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1); // инициализируем дисплей                                                                                                                              
+Adafruit_SSD1306 display(128, 32, &Wire, -1);  // инициализируем дисплей                                                                                                                              
 
-#define logo_width 40
-#define logo_height 40
+#define logo_width 20
+#define logo_height 20
 static const unsigned char PROGMEM logo[] = {
-  0xFF, 0xF7, 0x0F, 0xFF, 0x9F, 0xFF, 0xFF, 0x01, 0xF8, 0x8F, 0xFF, 0x07, 
-  0x00, 0xF0, 0x8F, 0xFF, 0x03, 0xF0, 0xE0, 0xDF, 0xFF, 0x00, 0x80, 0x03, 
-  0xFF, 0x3F, 0x30, 0x84, 0x03, 0xFE, 0x1F, 0x1C, 0xFC, 0x03, 0xFC, 0x0F, 
-  0x0F, 0x3C, 0x03, 0xF8, 0x86, 0x87, 0x3C, 0x13, 0xF0, 0xC4, 0xC7, 0x3F, 
-  0x33, 0xF0, 0xC0, 0xC7, 0x3F, 0x3E, 0xF0, 0xE0, 0x87, 0x3F, 0x0E, 0xF2, 
-  0xC6, 0x0F, 0xFE, 0x03, 0xF1, 0xC7, 0x0F, 0x80, 0x83, 0xF1, 0x87, 0x3F, 
-  0x00, 0xC3, 0xE0, 0x07, 0xFE, 0x01, 0x43, 0xE0, 0x07, 0xF8, 0x1F, 0x06, 
-  0xF0, 0x07, 0xC0, 0x3F, 0x06, 0xF0, 0x07, 0x00, 0x00, 0x0C, 0xF0, 0x07, 
-  0x00, 0x00, 0x0C, 0xF0, 0x07, 0xC0, 0x00, 0x0C, 0xF0, 0x07, 0x00, 0x00, 
-  0x0C, 0xF0, 0x07, 0x04, 0x00, 0x0C, 0xF0, 0x07, 0xE0, 0x41, 0x0C, 0xF0, 
-  0x07, 0x00, 0x00, 0x0C, 0xF0, 0x07, 0x04, 0x00, 0x04, 0xF0, 0x07, 0x60, 
-  0x00, 0x00, 0xE0, 0x07, 0x00, 0x00, 0x00, 0xE0, 0x07, 0x00, 0x00, 0x20, 
-  0xF0, 0x07, 0x70, 0x00, 0x06, 0xF0, 0x07, 0x00, 0x3E, 0x00, 0xE0, 0x07, 
-  0x02, 0x00, 0x00, 0xF0, 0x07, 0x18, 0x00, 0x0C, 0xF0, 0x0F, 0x80, 0xFF, 
-  0x00, 0xF0, 0x0F, 0x00, 0x00, 0x40, 0x78, 0x1F, 0x08, 0x00, 0x08, 0xFC, 
-  0x79, 0x40, 0xF9, 0x01, 0xBE, 0xF8, 0x00, 0x00, 0x80, 0x1F, 0xF0, 0x07, 
-  0x00, 0xF0, 0xE7, 0xF0, 0xFF, 0x80, 0xFF, 0xE7, };  // добавляем лого
+  0x00, 0x0C, 0x00, 0x80, 0x33, 0x00, 0xB0, 0xE7, 0x00, 0xC8, 0xE1, 0x01, 
+  0x44, 0x88, 0x02, 0x40, 0x80, 0x02, 0x84, 0xC2, 0x02, 0x0C, 0x6E, 0x03, 
+  0x1E, 0xC8, 0x03, 0xFC, 0xDF, 0x03, 0x7C, 0xD8, 0x03, 0x9C, 0xDF, 0x03, 
+  0xEC, 0xCF, 0x03, 0x7C, 0xF0, 0x03, 0x1C, 0x9F, 0x03, 0xFE, 0xFB, 0x03, 
+  0x3C, 0xCA, 0x03, 0xE8, 0xBF, 0x01, 0x70, 0x60, 0x00, 0x00, 0x1F, 0x00, };  // добавляем лого
 
  // словарь
 String words[] = {"подпись", "вырез", "гранит", "кругозор", "блузка", "фараон", "клапан", "ёж", "вымя", "турист", "колготки", "стоп-кран", "питание", "свёрток", "дочерь", "шампунь", "броня", "зайчатина", "гимназист", "стелька", "подделка", "виза", "затычка", "решение", "алкоголь", "шуруп", "воровка", "колодец", "кабан", "команда", "бордель", "ловушка", "буква", "опера", "сектор", "математика", "пароварка", "невезение", "глубина", "штука", "справочник", "вождь", "хобот", "ширинка", "усталость", "служитель", "жар", "спальная", "видео", "рот", "просьба", "фишка", "рукопись", "ракетчик", "каблук", "шрифт", "палец", "ножка", "халва", "черника", "незнайка", "компания", "работница", "мышь", "исследование", "кружка", "мороженое", "сиденье", "пулемёт", "печь", "солист", "свёкла", "стая", "зелье", "дума", "посылка", "коготь", "семафор", "брат", "различие", "плоскостопие", "двигатель", "сфера", "тюльпан", "затвор", "внедорожник", "самурай", "стан", "алгоритм", "параграф", "глаз", "медалист", "пульт", "поводок", "подлежащее", "ор", "бунт", "удочка", "лес", "диспетчер", "монитор", "вдова", "пиратство", "астролог", "сосед", "пуп", "изобретатель", "чума", "танец", "затишье", "пластелин", "йог", "маска", "блоха", "судьба", "сияние", "рукавица", "филе", "заплыв", "сёмга", "гиппопотам", "мастер", "походка", "ландыш", "яблоня", "кляча", "лиса", "свёртываемость", "раствор", "соты", "солод", "спорт", "шифер", "прощение", "стопка", "побег", "простота", "запах", "беседа", "варенье", "пароль", "актёр", "вырубка", "гвоздь", "шкаф", "скальпель", "гонг", "профессор", "казан", "скорбь", "извоз", "добавка", "тропа", "шеф", "космонавт", "грифель", "лауреат", "ромб", "борец", "звон", "канистра", "олимпиада", "оплата", "спирт", "баян", "перекрёсток", "влажность", "лотерея", "насморк", "оправдание", "мушкетёр", "мороз", "утюг", "санитария", "опрятность"};
-unsigned const int wordsSize = sizeof(words) / sizeof(String), soundPin = 13;  // размер словаря
+unsigned const int wordsSize = sizeof(words) / sizeof(String);  // размер словаря
 unsigned int rung = 1;
 unsigned long startTime = 0;
 bool check1 = false, timeGo = false;
@@ -46,13 +35,14 @@ void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // определяем дисплей по адресу 0x3C
   display.clearDisplay(); 
   display.cp437(true);  // задаем сдвиг под символам на вывод
+  display.setRotation(2);  // задаем поворот экрана
   display.setTextColor(WHITE); 
   delay(2000);
   
   display.drawXBitmap(128 / 2 - logo_width / 2, 0, logo, logo_width, logo_height, 1);
   display.display();
-  display.setTextSize(1,2);
-  display.setCursor(25, 45);
+  display.setTextSize(1);
+  display.setCursor(27, 25);
   display.write(80);  // P
   display.write(97);  // a
   display.write(110); // n
@@ -78,16 +68,16 @@ void loop() {
     Random(1);
   } // когда мы прошли весь словарь - решафлим его и пускаем заново
 
-  if(timeGo && millis() >= startTime + ESP8266TrueRandom.random(50000, 70000)){
+  if(timeGo && millis() >= startTime + 60000){
     startTime = 0;
     timeGo = false;
     display.clearDisplay();
-    display.drawRoundRect (1, 1, 127, 63, 5, 1);    
-    display.setTextSize(2);                  
-    display.setCursor(5, 15);
+    display.drawRoundRect (1, 1, 127, 31, 5, 1);    
+    display.setTextSize(1,2);                  
+    display.setCursor(25, 10);
     tone (soundPin, 2000, 200);
     display.println(utf8rus("Раунд"));
-    display.setCursor(5, 35);
+    display.setCursor(60, 10);
     display.println(utf8rus("окончен"));
     display.display();;
     delay(1500);
@@ -140,9 +130,9 @@ return target;
 
 void Output(int num){  // вывод слова на экран
   display.clearDisplay();
-  display.drawRoundRect (1, 1, 127, 63, 5, 1);    
-  display.setTextSize(2);                  
-  display.setCursor(5, 25);
+  display.drawRoundRect (1, 1, 127, 31, 5, 1);    
+  display.setTextSize(1,2);                  
+  display.setCursor(15, 10);
   display.println(utf8rus(words[num]));
   display.display();;
 }
