3354ad569190a85e8a0dfec7580f4a97f4e6e2df
Izack_Marck <isack5268@gmail.com>
2021-04-07 17:54:24 +0300

    update autolight

--- ESP8266/AutoLight/AutoLight.ino
+++ ESP8266/AutoLight/AutoLight.ino
@@ -1,48 +1,49 @@
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
-//#include <WiFiClient.h>
 
 #define rele_pin 4
 #define pyro_pin 12
 
-//char ssid[] = "";        
-//char pass[] = "";   
- 
-//WiFiClient client;
- 
-//const char server[] = "api.openweathermap.org";
-//String nameOfCity = "Moscow,RU"; 
-//String apiKey = ""; 
-
+TimeLord lord;
+char ssid[] = "none";        
+char pass[] = "none";   
+const float longitude = 37.61;
+const float latitude = 55.75;
 unsigned int timer;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(rele_pin, OUTPUT);
-  pinMode(pyro_pin, INPUT);
-  digitalWrite(rele_pin, LOW);
+  //pinMode(rele_pin, OUTPUT);
+  //pinMode(pyro_pin, INPUT);
+  //digitalWrite(rele_pin, LOW);
+  lord.TimeZone(-5 * 60);
+  lord.Position(26.9, -81.8);
+  lord.GMT(nowTime);
 
-  /*WiFi.begin(ssid, pass);
+ WiFi.begin(ssid, pass);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
   
-  Serial.println("WiFi Connected");
-  */
+  Serial.println("WiFi Connected"); 
 }
 
 void loop() {
-  if(digitalRead(pyro_pin) == 1){
-    Serial.println("1");
-    timer = millis();
-    digitalWrite(rele_pin, HIGH);
-  }
+  
+  /*if(lord.SunSet(nowTime)){
+    if(digitalRead(pyro_pin) == 1){
+      Serial.println("1");
+      timer = millis();
+      digitalWrite(rele_pin, HIGH);
+    }
 
-  if(millis() == timer + 60000){
-    Serial.println("2");
-    timer = 0;
-    digitalWrite(rele_pin, LOW);
-  }
+    if(millis() == timer + 60000){
+      Serial.println("2");
+      timer = 0;
+      digitalWrite(rele_pin, LOW);
+    }
+  }*/
+
+  delay(1000);
 }
