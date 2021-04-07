231e6cf416f6a824e9cb7a54c8787dc013a373e7
Izack_Marck <isack5268@gmail.com>
2021-04-07 18:23:41 +0300

    updated

--- ESP8266/AutoLight/AutoLight.ino
+++ ESP8266/AutoLight/AutoLight.ino
@@ -1,24 +1,23 @@
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 #define rele_pin 4
 #define pyro_pin 12
 
-TimeLord lord;
-char ssid[] = "none";        
-char pass[] = "none";   
-const float longitude = 37.61;
-const float latitude = 55.75;
+char ssid[] = "ssid";        
+char pass[] = "pass";   
+//const float longitude = 37.61;
+//const float latitude = 55.75;
 unsigned int timer;
 
 void setup() {
   Serial.begin(115200);
-  //pinMode(rele_pin, OUTPUT);
-  //pinMode(pyro_pin, INPUT);
-  //digitalWrite(rele_pin, LOW);
-  lord.TimeZone(-5 * 60);
-  lord.Position(26.9, -81.8);
-  lord.GMT(nowTime);
+  pinMode(rele_pin, OUTPUT);
+  pinMode(pyro_pin, INPUT);
+  digitalWrite(rele_pin, LOW);
 
  WiFi.begin(ssid, pass);
 
@@ -27,23 +26,55 @@ void setup() {
   }
   
   Serial.println("WiFi Connected"); 
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 }
 
 void loop() {
+  ArduinoOTA.handle();
   
-  /*if(lord.SunSet(nowTime)){
-    if(digitalRead(pyro_pin) == 1){
-      Serial.println("1");
-      timer = millis();
-      digitalWrite(rele_pin, HIGH);
-    }
+  if(digitalRead(pyro_pin) == 1){
+    Serial.println("1");
+    timer = millis();
+    digitalWrite(rele_pin, HIGH);
+  }
 
-    if(millis() == timer + 60000){
-      Serial.println("2");
-      timer = 0;
-      digitalWrite(rele_pin, LOW);
-    }
-  }*/
+  if(millis() == timer + 60000){
+    Serial.println("2");
+    timer = 0;
+    digitalWrite(rele_pin, LOW);
+  }
 
   delay(1000);
 }
